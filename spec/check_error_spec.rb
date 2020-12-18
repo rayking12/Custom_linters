c3abb6b03db3af07bf71b5157224edc957b09b67
unknown <rayodo667@gmail.com>
2020-12-18 17:56:56 +0100

    fixed context in rspecs

--- spec/check_error_spec.rb
+++ spec/check_error_spec.rb
@@ -4,58 +4,66 @@ describe Error do
   let(:checks) { Error.new('new.rb') }
 
   describe '#trailing_spaces' do
-    context 'when trailing spaces are found'
-    it 'should return trailing space error' do
-      checks.trailing_spaces
-      expect(checks.errors[0]).to eql('line:3:22: Error: Trailing whitespace detected.')
+    context 'when trailing spaces are found' do
+      it 'should return trailing space error' do
+        checks.trailing_spaces
+        expect(checks.errors[0]).to eql('line:3:22: Error: Trailing whitespace detected.')
+      end
     end
 
-    context 'when trailing spaces are not found'
-    it 'should not return trailing space error' do
-      checks.trailing_spaces
-      expect(checks.errors[0]).to_not eql('line:2:32: Error: Trailing whitespace detected.')
+    context 'when trailing spaces are not found' do
+      it 'should not return trailing space error' do
+        checks.trailing_spaces
+        expect(checks.errors[0]).to_not eql('line:2:32: Error: Trailing whitespace detected.')
+      end
     end
   end
 
   describe '#check_indentation' do
-    context 'when indentaton space is not correctly spaced'
-    it 'should return indentation space error on line 3' do
-      checks.check_indentation
-      expect(checks.errors[0]).to eql('line:10 Indentation Error: Use 2 spaces for indentation.')
+    context 'when indentaton space is not correctly spaced' do
+      it 'should return indentation space error on line 3' do
+        checks.check_indentation
+        expect(checks.errors[0]).to eql('line:10 Indentation Error: Use 2 spaces for indentation.')
+      end
     end
 
-    context 'when indentaton space is correctly spaced'
-    it 'should not return indentation space error on line 3' do
-      checks.check_indentation
-      expect(checks.errors[0]).to_not eql('line:2 Indentation Error: Use 2 spaces for indentation.')
+    context 'when indentaton space is correctly spaced' do
+      it 'should not return indentation space error on line 3' do
+        checks.check_indentation
+        expect(checks.errors[0]).to_not eql('line:2 Indentation Error: Use 2 spaces for indentation.')
+      end
     end
   end
 
   describe '#end_error' do
-    context "when there's an unexpected end"
-    it 'returns missing/unexpected end' do
-      checks.end_error
-      expect(checks.errors[0]).to eql("Missing 'end'")
+    context "when there's an unexpected end" do
+      it 'returns missing/unexpected end' do
+        checks.end_error
+        expect(checks.errors[0]).to eql("Missing 'end'")
+      end
     end
 
-    context "when there's an expected end"
-    it ' Does not returns missing/unexpected end' do
-      checks.end_error
-      expect(checks.errors[4]).to_not eql("Missing 'end'")
+    context "when there's an expected end" do
+      it ' Does not returns missing/unexpected end' do
+        checks.end_error
+        expect(checks.errors[4]).to_not eql("Missing 'end'")
+      end
     end
   end
 
   describe '#empty_line_error' do
-    context "when there's an empty line"
-    it 'returns empty line error' do
-      checks.empty_line_error
-      expect(checks.errors[0]).to eql('line:5 Extra empty line detected at block body end')
+    context "when there's an empty line" do
+      it 'returns empty line error' do
+        checks.empty_line_error
+        expect(checks.errors[0]).to eql('line:5 Extra empty line detected at block body end')
+      end
     end
 
-    context "when there's no empty line"
-    it ' does not returns empty line error' do
-      checks.empty_line_error
-      expect(checks.errors[0]).to_not eql('line:4 Extra empty line detected at block body end')
+    context "when there's no empty line" do
+      it ' does not returns empty line error' do
+        checks.empty_line_error
+        expect(checks.errors[0]).to_not eql('line:4 Extra empty line detected at block body end')
+      end
     end
   end
 end
